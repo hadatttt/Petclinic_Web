4af25c19dc6b38b6eb128d7745406ec4be7fbccc
Hà Văn Khánh Đạt <hagamer2004@gmail.com>
2025-01-05 10:00:00 +0700

    feat: add FakeService skeleton

--- src/main/java/org/springframework/samples/petclinic/sample/FakeService.java
+++ src/main/java/org/springframework/samples/petclinic/sample/FakeService.java
@@ -1,7 +1,1 @@
-package org.springframework.samples.petclinic.sample;
-
-public class FakeService {
-    public String sayHello() {
-        Helper.log("Hello Spring Framework!"); return "Hello Spring!";
-    }
-}
+// Empty Service
