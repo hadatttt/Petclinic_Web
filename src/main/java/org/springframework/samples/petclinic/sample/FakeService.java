7d211cf5e18e1f739a0a35da9b6b1b3f89e565eb
Hà Văn Khánh Đạt <hagamer2004@gmail.com>
2025-01-10 13:30:00 +0700

    refactor: update sayHello message

--- src/main/java/org/springframework/samples/petclinic/sample/FakeService.java
+++ src/main/java/org/springframework/samples/petclinic/sample/FakeService.java
@@ -2,6 +2,6 @@ package org.springframework.samples.petclinic.sample;
 
 public class FakeService {
     public String sayHello() {
-        Helper.log("Hello Spring!"); return "Hello Spring!";
+        Helper.log("Hello Spring Framework!"); return "Hello Spring!";
     }
 }
