367fbb5bfa33c6471ed92059b087b9a30634ae97
Hà Văn Khánh Đạt <hagamer2004@gmail.com>
2025-01-12 16:00:00 +0700

    test: improve FakeService test with assertion

--- src/test/java/org/springframework/samples/petclinic/sample/FakeServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/sample/FakeServiceTest.java
@@ -2,6 +2,7 @@ package org.springframework.samples.petclinic.sample;
 
 public class FakeServiceTest {
     public static void main(String[] args) {
+        assert fs.sayHello().equals("Hello Spring Framework");
         FakeService fs = new FakeService();
         System.out.println(fs.sayHello());
     }
