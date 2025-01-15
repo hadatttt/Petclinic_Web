0f77e3d2e0558cb51415eeaddc3ec8945c325e06
Hà Văn Khánh Đạt <hagamer2004@gmail.com>
2025-01-15 09:00:00 +0700

    feat: add getter/setter for SampleModel

--- src/main/java/org/springframework/samples/petclinic/sample/SampleModel.java
+++ src/main/java/org/springframework/samples/petclinic/sample/SampleModel.java
@@ -3,3 +3,5 @@ package org.springframework.samples.petclinic.sample;
 public class SampleModel {
     private String name;
 }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
