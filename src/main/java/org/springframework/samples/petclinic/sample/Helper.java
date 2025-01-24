fcc3f040fafa249016196db0a80ccb19cf407b78
Hà Văn Khánh Đạt <hagamer2004@gmail.com>
2025-01-24 13:00:00 +0700

    chore: log to stderr instead of stdout

--- src/main/java/org/springframework/samples/petclinic/sample/Helper.java
+++ src/main/java/org/springframework/samples/petclinic/sample/Helper.java
@@ -2,6 +2,6 @@ package org.springframework.samples.petclinic.sample;
 
 public class Helper {
     public static void log(String msg) {
-        System.out.println(msg);
+        System.err.println(msg);
     }
 }
